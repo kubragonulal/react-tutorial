af3f37da3da2bcef24b9f6612a31039835d4cc40
kubragonulal <kubragonulal@gmail.com>
2023-07-03 18:51:45 +0300

    Exercises in App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const test = 34;
+  const isAuth= true;
   return (
+
     <div className="App">
-      <h1>React App</h1>
+      <h4>{1+1}</h4>
+      <h4>{"Merhaba Dünya".toUpperCase()}</h4>
+      <h4>{new Date().getHours()}</h4>
+      <h4>{test}</h4>
+      <div>
+      {
+      isAuth ? <h4>Kullanıcı girişi yapıldı</h4>
+      : null
+    }
+      </div>
     </div>
+   
   );
 }
 
