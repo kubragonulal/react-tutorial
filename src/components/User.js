2e3819083ea30a998ba2f2c49853c4ebf14db30b
kubragonulal <kubragonulal@gmail.com>
2023-07-04 15:13:28 +0300

    User interface and Font Awesome.

--- src/components/User.js
+++ src/components/User.js
@@ -5,12 +5,18 @@ class User extends Component {
     //Destructing
     const {name, department, salary} = this.props;
     return (
-       <div>
-      <ul>
-      <li>İsim: {name}</li>
-      <li>Departman: {department}</li>
-      <li>Maaş: {salary}</li>
-      </ul>
+       <div calssName="col-md-8 mb-4">
+      <div className="card">
+        <div className="card-header d-flex justify-content-between">
+            <h4 className="d-inline">{name}</h4>
+            <i className="far fa-trash-alt" style={{cursor:"pointer"}}></i>
+            
+            </div>  
+            <div className="card-body"> 
+            <p className="card-text">Maaş : {salary}</p>  
+            <p className="card-text">Departman : {department}</p>
+            </div>
+        </div>    
     </div>
     )
   }
