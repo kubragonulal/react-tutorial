98aa88eaa90994a77435006c7e924381cec63063
kubragonulal <kubragonulal@gmail.com>
2023-07-07 15:10:44 +0300

    React Events.

--- src/components/User.js
+++ src/components/User.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react'
 import PropTypes from "prop-types";
 class User extends Component {
+    //State oluşturmanın başka bir yolu
+    state = {
+        isVisible : false
+    }
+    // Bind işlemi başka yolu
     constructor(props){
         super(props);
-        this.state = {
-            isVisible : false
-        }
+        this.onClickEvent = this.onClickEvent.bind(this);
     }
+    
+   onClickEvent = (number,e) => {
+    console.log(number);
+   }
+
   render() {
     //Destructing
     const {name, department, salary} = this.props;
     const {isVisible} = this.state;    
     return (
-       <div calssName="col-md-8 mb-4">
+       <div className="col-md-8 mb-4">
       <div className="card">
         <div className="card-header d-flex justify-content-between">
-            <h4 className="d-inline">{name}</h4>
+            <h4 className="d-inline" onClick = {this.onClickEvent.bind(this,61)}>{name}</h4>
+
             <i className="far fa-trash-alt" style={{cursor:"pointer"}}></i>
             
             </div>  
@@ -23,7 +32,6 @@ class User extends Component {
                 isVisible ? <div className="card-body"> 
                 <p className="card-text">Maaş : {salary}</p>  
                 <p className="card-text">Departman : {department}</p>
-                <p>{this.state.test}</p>
                 </div> : null
             }
             
